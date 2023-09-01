10813b5cdf46620d5981853c8162e5e5895c4caa
Kazzywiz <ayomidekay7@gmail.com>
2023-09-01 01:18:52 +0000

    Print to STDOUT with printf.

---
+++ 0x00C-HelloWorld/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print out string with printf
+ * Return: 0
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
