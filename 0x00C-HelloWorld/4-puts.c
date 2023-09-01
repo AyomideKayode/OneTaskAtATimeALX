666cd6ff56180b73f352caa49cf086186d14e2bb
Kazzywiz <ayomidekay7@gmail.com>
2023-09-01 01:18:24 +0000

    Print to STDOUT with puts.

---
+++ 0x00C-HelloWorld/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * use puts to print out words
+ * Return: 0
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
