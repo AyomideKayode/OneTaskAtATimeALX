e6fea071597faf11113e8771db9d1549a7f79c41
Kazzywiz <ayomidekay7@gmail.com>
2023-10-07 17:23:09 +0100

    Tasks 7 & 8

---
+++ 0x00C-HelloWorld/8-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Description: function that prints a string
+  * with special character
+  * Return: 1 if sucessful
+  */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - DOra Korpar, 2015-10-19\n");
+	return (1);
+}
+
